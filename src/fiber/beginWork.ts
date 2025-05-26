c71dad34c97798ef1ab37cf3299439b3f09c9695
kimyh93 <kimyh93@rexgen.co.kr>
2025-05-26 22:21:41 +0900

    feat: beginWork, compleletWork 함수 diff 기능 추가
    Desc
    - 기존에 구현한 begin, complete Work 함수에 이중 버퍼 구조를 적용해서 비교 작업을 수행하는 diff 관련 로직 추가

--- src/fiber/beginWork.ts
+++ src/fiber/beginWork.ts
@@ -1,12 +1,21 @@
 import { FiberNode } from "./type.fiber";
 
 export function beginWork(fiber: FiberNode): void {
-  if (typeof fiber.type === "string" && !fiber.stateNode) {
+  const prevFiber = fiber.alternate;
+
+  const isHostComponent = typeof fiber.type === "string";
+  const hasDOM = fiber.stateNode !== null;
+  const propsChanged =
+    prevFiber && prevFiber.memoizedProps !== prevFiber.pendingProps;
+
+  if (isHostComponent && !hasDOM) {
     fiber.flags = "Placement";
     console.log(`[beginWork] ${fiber.type} → flags = Placement`);
+  } else if (isHostComponent && propsChanged) {
+    fiber.flags = "Update";
+    console.log(`[beginWork] ${fiber.type} → flags = Update`);
   }
 
   if (fiber.child) beginWork(fiber.child);
-
   if (fiber.sibling) beginWork(fiber.sibling);
 }
