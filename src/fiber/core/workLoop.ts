25dcfd93d96d9f5cdd9050c27e771cfbbaea15c4
mac <mac@macs-MacBook-Pro.local>
2025-06-29 12:31:29 +0900

    refactor: workLoop 함수 반환값 설정
    Desc
    - while-loop를 통해 render 처리를 완료한 fiber 트리를 반환하도록 수정

--- src/fiber/core/workLoop.ts
+++ src/fiber/core/workLoop.ts
@@ -1,10 +1,12 @@
 import { FiberNode } from "../type.fiber";
 import { performUnitOfWork } from "./performUnitOfWork";
 
-export function workLoop(fiber: FiberNode | null): void {
+export function workLoop(fiber: FiberNode | null): FiberNode | null {
   let workInProgress: FiberNode | null = fiber;
 
   while (workInProgress !== null) {
     workInProgress = performUnitOfWork(workInProgress);
   }
+
+  return fiber;
 }
